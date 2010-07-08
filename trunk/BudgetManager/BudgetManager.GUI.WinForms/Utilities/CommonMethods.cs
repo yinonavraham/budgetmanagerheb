2654bcc88e1b0ad1709a98c808b56cc060271a95
yinonavraham <yinonavraham@1abe7bff-0239-863e-2933-61e32059d068>
2010-07-08 07:42:41 +0000

    Add methods for getting darker / lighter versions of a color

--- trunk/BudgetManager/BudgetManager.GUI.WinForms/Utilities/CommonMethods.cs
+++ trunk/BudgetManager/BudgetManager.GUI.WinForms/Utilities/CommonMethods.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using BudgetManager.Data;
+using System.Drawing;
 
 namespace BudgetManager.GUI.WinForms.Utilities
 {
@@ -39,6 +40,26 @@ namespace BudgetManager.GUI.WinForms.Utilities
                 FillCategoriesTreeNode(ref newNode, cat);
                 node.Nodes.Add(newNode);
             }
+        }
+
+        public static Color GetDarkerColor(Color color)
+        {
+            int r, g, b, d;
+            d = 40;
+            r = color.R - d < 0 ? 0 : color.R - d;
+            g = color.G - d < 0 ? 0 : color.G - d;
+            b = color.B - d < 0 ? 0 : color.B - d;
+            return Color.FromArgb(r, g, b);
+        }
+
+        public static Color GetLighterColor(Color color)
+        {
+            int r, g, b, d;
+            d = 40;
+            r = color.R + d > 255 ? 255 : color.R + d;
+            g = color.G + d > 255 ? 255 : color.G + d;
+            b = color.B + d > 255 ? 255 : color.B + d;
+            return Color.FromArgb(r, g, b);
         }
     }
 }
